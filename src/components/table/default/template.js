408cbc7b6a1f620f42ab28946e3a759324dd154d
Aleksey Tihonravov <fosterfosterfoster63@gmail.com>
2023-08-14 17:48:42 +0400

    initial pagination

--- src/components/table/default/template.js
+++ src/components/table/default/template.js
@@ -82,23 +82,48 @@ const template = `
       </tbody>
     </table>
     <div class="v-table-footer">
+      <div class="v-table-footer-total">
+        Итого
+      </div>
       <div class="v-table-footer-pagination">
         <div class="v-table-footer-pagination-length">
-          <span>
+          <p class="v-table-footer-pagination-length__label">Количество на странице:</p>
+          <!--<span>
             10
-          </span>
+          </span>-->
+          <select name="" id="">
+            <option v-for="(option, optionIndex) in 5" value="">10</option>
+          </select>
         </div>
         <div class="v-table-footer-pagination-wrap">
-          <div class="">
-            button
+          <div class="v-table-footer-pagination__button v-table-footer-pagination__button--prev">
+            <svg height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 185.343 185.343" xml:space="preserve">
+              <g>
+                <g>
+                  <path fill="currentColor" d="M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175
+                    l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934
+                    c4.194,4.194,4.194,10.987,0,15.175l-81.934,81.939C57.201,184.293,54.454,185.343,51.707,185.343z"/>
+                </g>
+              </g>
+            </svg>
           </div>
           <div class="v-table-footer-pagination-pages">
-            <div v-for="item, index in 5"  class="">
-              {{ index }}
+            <div v-for="item, index in 5" :class="index === 2 ? 'v-table-footer-pagination-pages__el--active' : ''"  class="v-table-footer-pagination-pages__el v-table-footer-pagination__button">
+              <span>{{ index }}</span>
             </div>
           </div>
-          <div class="">
-            button
+          <div class="v-table-footer-pagination__button v-table-footer-pagination__button--next">
+            <svg fill="currentColor" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 185.343 185.343" xml:space="preserve">
+              <g>
+                <g>
+                  <path fill="currentColor" d="M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175
+                    l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934
+                    c4.194,4.194,4.194,10.987,0,15.175l-81.934,81.939C57.201,184.293,54.454,185.343,51.707,185.343z"/>
+                </g>
+              </g>
+            </svg>
           </div>
         </div>
       </div>
